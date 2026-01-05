461988e88ce675275cc83feb44864d9dbb9ca8f9
Erick Rodrigues <58482037+erickerk@users.noreply.github.com>
2026-01-05 10:33:22 -0300

    Desabilitar otimização de CSS experimental para compatibilidade com Vercel

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     removeConsole: process.env.NODE_ENV === "production",
   },
   experimental: {
-    optimizeCss: true,
+    optimizeCss: false,
   },
 
   // Configuração de imagens
